9027c2a3f5329d11d14afe1aec6c5f80dc601832
wpmohammad1 <alimohammadjhd@gmail.com>
2023-03-03 11:42:35 +0600

    added details button

--- js/app.js
+++ js/app.js
@@ -6,25 +6,44 @@ const loadTools = async() =>{
 }
 
 const displayTools = tools =>{
-    console.log(tools)
+
     const toolsContainer = document.getElementById('tool-container');
+
+    // display all features
     tools.tools.forEach(tool => {
         const toolDiv = document.createElement('div')
         toolDiv.classList.add('col')
         toolDiv.innerHTML =`
         <div class="card h-100">
-        <img src="" class="card-img-top p-4" alt="...">
+        <img src="${tool.image}" class="card-img-top p-4" alt="...">
             <div class="card-body">
-                <h5 class="card-title"></h5>
-                <p class="card-text"></p>
+                <h5 class="card-title">Features</h5>
+                <p class="card-text">1. ${tool.features[0]? tool.features[0]: 'No Details'}</p>
+                <p class="card-text">2. ${tool.features[1]? tool.features[1]: 'No Details'}</p>
+                <p class="card-text">3. ${tool.features[2]? tool.features[2]: 'No Details'}</p>
             </div>
                 <div class="card-footer">
-                    <small class="text-muted">Last updated 3 mins ago</small>
+                    <div class="d-flex justify-content-between mt-4">
+                    <div>
+                    <h4>${tool.name}</h4>
+                    <p>${tool.published_in}</P>
+                    </div>
+
+                    <div>
+                    <i class="fas fa-arrow-right" onclick="showDetails('${tools.id}')" style="font-size:25px; background-color: antiquewhite; border-radius: 50%;"></i>
+                    
+                    </div>
+                    </div>
                 </div>
         </div>
         `
         toolsContainer.appendChild(toolDiv)
     });
 }
-
+const showDetails = id =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tool/${id}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data))
+}
 loadTools();
