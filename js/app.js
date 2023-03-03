0c8b8b325aa1ebf3abacae7033f9f6f58308e33a
wpmohammad1 <alimohammadjhd@gmail.com>
2023-03-03 18:54:57 +0600

    create modal concepts and others

--- js/app.js
+++ js/app.js
@@ -8,8 +8,18 @@ const loadTools = async() =>{
 const displayTools = tools =>{
 
     const toolsContainer = document.getElementById('tool-container');
-    
+    // display 6 tools only
+    const showAll = document.getElementById('show-all')
+
+    if(tools.length > 6){
+        features = features.slice(0, 6)
+        showAll.classList.remove('d-none')
+    }
+    else{
+        showAll.classList.add('d-none')
+    }
     // display all features
+    // start loader
     toggleSpinner(true);
     tools.tools.forEach(tool => {
         const toolDiv = document.createElement('div')
@@ -25,15 +35,17 @@ const displayTools = tools =>{
             </div>
                 <div class="card-footer">
                     <div class="d-flex justify-content-between mt-4">
-                    <div>
-                    <h4>${tool.name}</h4>
-                    <p>${tool.published_in}</P>
-                    </div>
-
-                    <div>
-                    <i class="fas fa-arrow-right text-danger" onclick="showDetails('${tools.id}')" style="font-size:25px; background-color: antiquewhite; border-radius: 50%;"></i>
-                    
-                    </div>
+                        <div>
+                            <h4>${tool.name}</h4>
+                            <div class="d-flex">
+                                <div><i class="fa-solid fa-calendar-days m-2"></i></div>
+                                <p>${tool.published_in}</P>
+                            </div>
+                        </div>
+                        <div>
+                            <i class="fas fa-arrow-right text-danger" onclick="showDetails('${tools.id}')" data-bs-toggle="modal" data-bs-target="#showDetailModal" style="font-size:25px; background-color: antiquewhite; border-radius: 50%;"></i>
+                            
+                        </div>
                     </div>
                 </div>
         </div>
@@ -43,13 +55,16 @@ const displayTools = tools =>{
     // stop loader 
     toggleSpinner(false)
 }
-const showDetails = id =>{
-    const url = `https://openapi.programming-hero.com/api/ai/tools/${id}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data))
+const showDetails =async id =>{
+    const url = `https://openapi.programming-hero.com/api/ai/tools/'${id}'`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayDetails(data.data)
 }
-//  start loader
+const displayDetails = details =>{
+    console.log(details)
+}
+//  display loader 
 const toggleSpinner = isLoading =>{
     const loaderSection = document.getElementById('loader')
     if(isLoading){
@@ -59,4 +74,5 @@ const toggleSpinner = isLoading =>{
         loaderSection.classList.add('d-none')
     }
 }
+
 loadTools();
